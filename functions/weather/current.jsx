6c9674b9b9ebdfe3f8bd27e269ffb49b446101ed
Andre Goncalves <andrepcg@gmail.com>
2024-09-15 14:17:01 +0100

    Wind direction

--- functions/weather/current.jsx
+++ functions/weather/current.jsx
@@ -9,14 +9,22 @@ async function getWeather(api_key) {
 }
 
 function windDirection(deg) {
-  if (deg >= 337.5 || deg < 22.5) return "N";
-  if (deg >= 22.5 && deg < 67.5) return "NE";
-  if (deg >= 67.5 && deg < 112.5) return "E";
-  if (deg >= 112.5 && deg < 157.5) return "SE";
-  if (deg >= 157.5 && deg < 202.5) return "S";
-  if (deg >= 202.5 && deg < 247.5) return "SW";
-  if (deg >= 247.5 && deg < 292.5) return "W";
-  if (deg >= 292.5 && deg < 337.5) return "NW";
+  let dir;
+  if (deg >= 337.5 || deg < 22.5) dir = "N";
+  if (deg >= 22.5 && deg < 67.5) dir = "NE";
+  if (deg >= 67.5 && deg < 112.5) dir = "E";
+  if (deg >= 112.5 && deg < 157.5) dir = "SE";
+  if (deg >= 157.5 && deg < 202.5) dir = "S";
+  if (deg >= 202.5 && deg < 247.5) dir = "SW";
+  if (deg >= 247.5 && deg < 292.5) dir = "W";
+  if (deg >= 292.5 && deg < 337.5) dir = "NW";
+
+  return (
+    <span>
+      {dir}
+      <span style={{ transform: `rotate(${deg}deg)` }}>⬇</span>
+    </span>
+  );
 }
 
 function formatDate(epoch) {
